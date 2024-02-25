b78e055decc3ee14375d84252f75728c6f511663
Uday Singh <uday17631@gmail.com>
2024-02-25 10:55:13 +0530

    DSA Day 1 Commit

---
+++ Day 1/dsa_day_1.py
@@ -0,0 +1,103 @@
+#Day 1
+# Introductin to Binary search and complexity Analysis with Python
+
+# QUESTION 1: Alice has some cards with numbers written on them. She arranges the cards in decreasing order, and lays them out face down in a sequence on a table. She challenges Bob to pick out the card containing a given number by turning over as few cards as possible. Write a function to help Bob locate the card.
+
+# Ans:
+# Sorted List -> [13, 11, 12, 7, 4, 3, 1, 0] e.g
+# Positions ->   [  0   1   2  3  4  5  6  7 ]
+
+# Example
+# cards = [13, 11, 10, 7, 4, 3, 1, 0]
+# query = 7
+# output = 3
+
+# The problem statement does not specify what to do if the list cards does not contain the number query.
+
+# 1.) Read the problem statement again, carefully.
+# 2.) Look through the examples provided with the problem.
+# 3.) Ask the interviewer/platform for a clarification.
+# 4.) Make a reasonable assumption, state it and move forward.
+
+# Self made Test cases.
+tests = []
+
+# Test Case 1
+tests.append({
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 7
+    },
+    'output': 3
+})
+
+# Test Case 2
+tests.append({'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0})
+
+# Test Case 3
+tests.append({
+    'input': {
+        'cards': [3, -1, -9, -127],
+        'query': -127
+    },
+    'output': 3
+})
+
+# Test Case 3
+tests.append({'input': {'cards': [6], 'query': 6}, 'output': 0})
+
+# Test Case 4
+tests.append({
+    'input': {
+        'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query': 3
+    },
+    'output': 7
+})
+
+# Linear Search (Brute force)
+
+# 1.) Create a variable position with the value 0.
+# 2.) Check whether the number at index position in card equals query.
+# 3.) If it does, position is the answer and can be returned from the function
+# 4.) If not, increment the value of position by 1, and repeat steps 2 to 5 till we reach the last position.
+# 5.) If the number was not found, return -1.
+
+
+def locate_card(cards, query):
+  # Create a variable position with the value 0
+  position = 0
+  # Set up a loop for repetition
+  while True:
+    # Check if the element at the current postion matche the query
+    if cards[position] == query:
+      #Answer found! Return and exit
+      return position
+
+    # Increament the position
+    position += 1
+
+    # Check if we have reached the end of the array
+    if position == len(cards):
+      # Number not found, Return -1
+      return -1
+
+
+
+# Use this run_tests() function for all other test run cases to check pass and failed
+
+def run_tests():
+  for i, test in enumerate(tests, 1):
+    input_data = test['input']
+    expected_output = test['output']
+
+    result = locate_card(**input_data)
+
+    if result == expected_output:
+      print(f'Test case {i}: Passed')
+    else:
+      print(f'Test case {i}: Failed. Expected {expected_output}, got {result}')
+
+
+# Run the tests
+run_tests()
